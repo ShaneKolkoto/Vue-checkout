e4df96a3a6008b8fcf3678ec611ce7a08ec43615
ShaneKolkoto <shanemorne@gmail.com>
2023-01-11 12:01:35 +0000

    fixed and updated

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,13 +17,13 @@
       <div class="card-item__wrapper">
         <div class="card-item__top">
           <img
-            src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
+            src="https://raw.githubusercontent.com/ShaneKolkoto/Vue-checkout/master/src/assets/images/chip.png"
             class="card-item__chip"
           />
           <div class="card-item__type">
             <transition name="slide-fade-up">
               <img
-                :src="'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' + cardType + '.png'"
+                :src="'https://raw.githubusercontent.com/ShaneKolkoto/Vue-checkout/master/src/assets/images/' + cardType + '.png'"
                 v-if="cardType"
                 :key="cardType"
                 alt
@@ -104,7 +104,7 @@
         </div>
         <div class="card-item__type">
           <img
-            :src="'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' + cardType + '.png'"
+            :src="'https://raw.githubusercontent.com/ShaneKolkoto/Vue-checkout/master/src/assets/images/' + cardType + '.png'"
             v-if="cardType"
             class="card-item__typeImg"
           />
@@ -209,7 +209,7 @@ export default {
     currentCardBackground () {
       if (this.randomBackgrounds && !this.backgroundImage) { // TODO will be optimized
         let random = Math.floor(Math.random() * 25 + 1)
-        return `https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/${random}.jpeg`
+        return `https://raw.githubusercontent.com/ShaneKolkoto/Vue-checkout/master/src/assets/images/${random}.jpeg`
       } else if (this.backgroundImage) {
         return this.backgroundImage
       } else {
